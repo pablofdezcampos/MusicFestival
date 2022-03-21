3ad8b011e13d31accdc0d3c78fb1cae21fd034e4
Pablo Fernández Campos <pablofdezcvc1@hotmail.com>
2022-03-21 16:28:07 +0100

    Adding Gallery Section with JavaScript

--- build/js/app.js
+++ build/js/app.js
@@ -8,5 +8,14 @@ function startApp() {
 
 function createGallery() {
     const gallery = document.querySelector('.images-gallery');
-
+    for (let i = 1; i <= 12; i++) {
+        const image = document.createElement('picture');
+        image.innerHTML = `
+        <source srcset="build/img/thumb/${i}.avif" type="image/avif">
+        <source srcset="build/img/thumb/${i}.webp" type="image/webp">
+        <img width="200" height="300" loading="lazy" src="build/img/thumb/${i}.jpg" 
+        alt="Vocalist image">
+        `;
+        gallery.appendChild(image);
+    }
 }
